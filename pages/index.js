b684c904f2d468ff48d3b6c8e74aeacc8febfb69
halosatrio <63773815+halosatrio@users.noreply.github.com>
2021-02-22 14:08:11 +0700

    bikin komponent awal

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import Head from "next/head";
+import Card from "../components/Card";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await axios
+        .get("https://5f50ca542b5a260016e8bfb0.mockapi.io/api/v1/movies")
+        .then(function (res) {
+          setData(res.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(setLoading(false));
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>LinkAja - Movies Database</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <body className="py-8 px-3">
+        <h1 className="mb-8 text-4xl">Link aja Movies Database</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {data.map((item) => (
+            <Card
+              className="rounded-lg border-black border-2 p-4 text-center"
+              key={item.id}
+            >
+              {item.title}
+            </Card>
+          ))}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      </body>
+    </>
+  );
 }

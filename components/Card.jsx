4d42835270d45bf15b4f6ec9186bc55e8ba543a4
halosatrio <63773815+halosatrio@users.noreply.github.com>
2021-02-23 17:01:28 +0700

    add and testing loadable for react lazy-load component

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,32 +1,11 @@
-// import Image from "next/image"
-// import { useEffect, useState } from "react";
-
 const Card = (props) => {
-  // const [image, setImage] = useState();
-  
-  // useEffect(() => {
-  //     const fetchImage = async () => {
-  //       await axios
-  //         .get(`https://picsum.photos/id/${props.uid}/200/300`)
-  //         .then(function (res) {
-  //           setImage(res.data);
-  //         })
-  //         .catch(function (error) {
-  //           console.log(error);
-  //         })
-  //     };
-  //     fetchImage();
-  //   }, []);
-
   const showTime = new Date(props.showTime).toLocaleDateString()
-
+  
   return (
     <div className="relative">
       <div className="mx-auto h-80">
-        <img src={`https://picsum.photos/id/${props.uid}/200/300`} alt="Movies" className="object-cover h-full w-full  rounded-lg" loading="lazy" />
+        <img src={`https://picsum.photos/id/${props.uid}/200/300`} alt="Movies" className="object-cover h-full w-full rounded-lg" loading="lazy" />
       </div>
-
-      {/* <Image src={image} alt={`movies ${props.uid}`} width={500} height={500} loading="lazy" /> */}
 
       <div className="absolute bottom-0 right-0 left-0 bg-white bg-opacity-80 rounded-b-lg">
         <p className="text-center mt-3">
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,40 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import loadable from "@loadable/component";
 
 import Head from "next/head";
-import Card from "../components/Card";
+// import Card from "../components/Card";
 import SearchBar from "../components/SearchBar";
 
+const Card = loadable(() => import("../components/Card"));
+
 export default function Home() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  // const data = [
-  //   { title: "Hakuna Matata", id: 1, showTime: new Date() },
-  //   { title: "Lion King", id: 2, showTime: new Date() },
-  //   { title: "Hercules", id: 3, showTime: new Date() },
-  // ];
+  const item = [
+    { title: "Hakuna Matata", id: 1, showTime: new Date() },
+    { title: "Lion King", id: 2, showTime: new Date() },
+    { title: "Hercules", id: 3, showTime: new Date() },
+    { title: "Hakuna Matata 2", id: 4, showTime: new Date() },
+    { title: "Lion King 2", id: 5, showTime: new Date() },
+    { title: "Hercules 2", id: 6, showTime: new Date() },
+  ];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      await axios
-        .get("https://5f50ca542b5a260016e8bfb0.mockapi.io/api/v1/movies")
-        .then(function (res) {
-          setData(res.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-        .then(setLoading(false));
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     await axios
+  //       .get("https://5f50ca542b5a260016e8bfb0.mockapi.io/api/v1/movies")
+  //       .then(function (res) {
+  //         setData(res.data);
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       })
+  //       .then(setLoading(false));
+  //   };
+  //   fetchData();
+  // }, []);
 
   const searching = (event) => {
     setSearchTerm(event.target.value);
@@ -44,10 +49,48 @@ export default function Home() {
         <h1 className="text-center mb-8 text-4xl">Link aja Movies Database</h1>
         <SearchBar searching={searching} />
         {loading ? (
-          <h1 className="text-xl text-center">Loading...</h1>
+          <h1 className="mb-8 text-3xl text-center">Loading...</h1>
         ) : (
+          // <h1 className="mb-8 text-3xl text-center">Success</h1>
+
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {data
+            <Card
+              key={item[0].id}
+              title={item[0].title}
+              showTime={item[0].showTime}
+              uid={item[0].id}
+            />
+            <Card
+              key={item[1].id}
+              title={item[1].title}
+              showTime={item[1].showTime}
+              uid={item[1].id}
+            />
+            <Card
+              key={item[2].id}
+              title={item[2].title}
+              showTime={item[2].showTime}
+              uid={item[2].id}
+            />
+            <Card
+              key={item[3].id}
+              title={item[3].title}
+              showTime={item[3].showTime}
+              uid={item[3].id}
+            />
+            <Card
+              key={item[4].id}
+              title={item[4].title}
+              showTime={item[4].showTime}
+              uid={item[4].id}
+            />
+            <Card
+              key={item[5].id}
+              title={item[5].title}
+              showTime={item[5].showTime}
+              uid={item[5].id}
+            />
+            {/* {data
               .filter((val) => {
                 if (searchTerm == "") {
                   return val;
@@ -64,7 +107,7 @@ export default function Home() {
                   showTime={item.showTime}
                   uid={item.id}
                 />
-              ))}
+              ))} */}
           </div>
         )}
       </div>

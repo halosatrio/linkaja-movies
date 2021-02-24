a560b97b6ff7959104b90b52bf9780dc68ee8fbe
halosatrio <63773815+halosatrio@users.noreply.github.com>
2021-02-24 10:01:19 +0700

    fix autocomplete clear text field onSubmit

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -10,14 +10,15 @@ const SearchBar = (props) => {
   };
 
   const handleSubmit = (event) => {
-    onMovieSelect(searchTerm)
     event.preventDefault();
+    onMovieSelect(searchTerm)
+    setSearchTerm("")
   }
 
   return (
     <div>
      <form className="bg-white shadow-sm p-4 flex" onSubmit={handleSubmit}>
-        <input className="w-full rounded p-2" type="text" placeholder="Search Movies" onChange={searching} />
+        <input className="w-full rounded p-2" type="text" placeholder="Search Movies" value={searchTerm} onChange={searching} />
         <button type="submit" className="bg-red-400 hover:bg-red-300 rounded text-white p-2 pl-4 pr-4">
           <p className="font-semibold text-xs">Search</p>
         </button>

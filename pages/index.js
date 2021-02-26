3fba4bb7a45ff36e81d229c5b85939fc25221fe2
halosatrio <63773815+halosatrio@users.noreply.github.com>
2021-02-26 15:18:47 +0700

    fix date filter

--- pages/index.js
+++ pages/index.js
@@ -13,23 +13,46 @@ import Footer from "../components/Footer";
 export default function Home({ data }) {
   const [selectedMovie, setSelectedMovie] = useState("");
   const [filtered, setFiltered] = useState([]);
-  const [startDate, setStartDate] = useState(new Date("2020/01/01"));
+  const [startDate, setStartDate] = useState(new Date("2020 01 01"));
+  const [selectDate, setSelectDate] = useState();
 
   const onMovieSelect = (movie) => {
     setSelectedMovie(movie);
   };
 
+  const onDateSelect = (date) => {
+    setSelectDate(date);
+    setStartDate(date);
+  };
+
   useEffect(() => {
-    if (selectedMovie !== "") {
+    handleFiltered(selectedMovie, selectDate);
+  }, [selectedMovie, selectDate]);
+
+  const handleFiltered = (movie, date) => {
+    if (movie) {
+      setSelectDate();
       setFiltered(
         data.filter((val) =>
-          val.title.toLowerCase().includes(selectedMovie.toLowerCase())
+          val.title.toLowerCase().includes(movie.toLowerCase())
         )
       );
-    } else {
-      setFiltered([]);
     }
-  }, [selectedMovie]);
+    if (date) {
+      setSelectedMovie();
+      setFiltered(
+        data.filter(
+          (item) =>
+            dayjs(item.showTime).format("DD MM YYYY") ==
+            dayjs(date).format("DD MM YYYY")
+        )
+      );
+    }
+    if (movie == "") {
+      setSelectDate();
+      setFiltered(data);
+    }
+  };
 
   return (
     <>
@@ -47,7 +70,7 @@ export default function Home({ data }) {
           <h3 className="text-md mr-3">Filter movies by show time: </h3>
           <DatePicker
             selected={startDate}
-            onChange={(date) => setStartDate(date)}
+            onChange={onDateSelect}
             className="py-1 text-sm text-center bg-red-200 rounded-md cursor-pointer"
           />
         </div>
@@ -55,28 +78,19 @@ export default function Home({ data }) {
           <h1 className="mb-8 text-3xl text-center">Loading...</h1>
         ) : (
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
-            {data
-              .filter((val) => {
-                if (selectedMovie == "") {
-                  return val;
-                } else if (
-                  val.title.toLowerCase().includes(selectedMovie.toLowerCase())
-                ) {
-                  return val;
-                }
-              })
-              .map((item1) => (
-                <Card
-                  key={item1.id}
-                  image={item1.image}
-                  uid={item1.id}
-                  showTime={item1.showTime}
-                />
-              ))}
+            {filtered.map((item1) => (
+              <Card
+                key={item1.id}
+                title={item1.title}
+                image={item1.image}
+                uid={item1.id}
+                showTime={item1.showTime}
+              />
+            ))}
           </div>
         )}
       </div>
-      {selectedMovie && filtered == false && (
+      {filtered == false && (
         <div className="mx-auto mt-2">
           <h1 className="text-4xl text-center font-bold">Movies Not Found</h1>
         </div>
